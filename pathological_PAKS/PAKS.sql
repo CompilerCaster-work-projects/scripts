5157f01efb3241f9fcf4e72f5622563c597bb604
CompilerCaster <job.kuzmingleb@gmail.com>
2024-05-23 16:54:43 +0800

    add files

---
+++ pathological_PAKS/PAKS.sql
@@ -0,0 +1,34 @@
+select
+h.license as "ПАК", avg(pulse::float) as "Средний пульс", stddev_pop(pulse::float) as "Ст.откл пульса",
+avg(ad_sys::float) as "Среднее сАД", stddev_pop(ad_sys::float) as "Ст.откл сАД",
+avg(ad_dis::float) as "Среднее дАД", stddev_pop(ad_dis::float) as "Ст.откл дАД",
+avg(temperature::float) as "Средняя температура", stddev_pop(temperature::float) as "Ст.откл температуры",
+count(*)
+from processing.inspections_pool_resolved a
+inner join structures.hosts h
+on a.host_id = h.id
+inner join structures.employees e
+on e.id = a.employee_id
+inner join (SELECT
+json_data->'result'->'value'->>'pulse' AS pulse,
+json_data->'result'->'value'->'pressure'->>'systolic' AS ad_sys,
+json_data->'result'->'value'->'pressure'->>'diastolic' AS ad_dis,
+ipr.id as "id"
+FROM
+processing.inspections_pool_resolved ipr ,
+jsonb_array_elements(ipr.steps) AS json_data
+WHERE
+json_data->>'type' = 'tonometry') t
+on t.id = a.id
+inner join (SELECT
+json_data->'result'->>'value' AS temperature,
+ipr.id as id
+FROM
+processing.inspections_pool_resolved ipr ,
+jsonb_array_elements(ipr.steps) AS json_data
+WHERE
+json_data->>'type' = 'temperature') t1
+on t1.id = a.id
+where a.started_at between '2023-12-01' and '2023-12-31'
+group by  
+h.license;
